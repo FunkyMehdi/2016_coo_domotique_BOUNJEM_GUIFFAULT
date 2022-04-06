3471d089a3310fa3b6c4c084defbc2cf3b21b6f2
Guiffault Elian <guiffaul2u@etu.univ-lorraine.fr>
2022-04-06 11:37:53 +0200

    Telecommande crée Elian et Mehdi

--- Lampe.java
+++ Lampe.java
@@ -14,4 +14,31 @@ public class Lampe {
     public void eteindre(){
         this.allume = false;
     }
+
+    public String toString (){
+        String aff;
+        aff = "nom de la lampe :" + this.nom + "\n";
+        if (!this.allume){
+            aff += "Off";
+        } else{
+            aff+= "On";
+        }
+        return aff;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public boolean isAllume() {
+        return allume;
+    }
+
+    public void setAllume(boolean allume) {
+        this.allume = allume;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
 }
---
+++ Telecommande.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Telecommande {
+    private List<Lampe> lampes;
+
+    public Telecommande (){
+        List<Lampe> l = new ArrayList<>();
+    }
+
+    public void ajouterLampe(Lampe lampe){
+        this.lampes.add(lampe);
+    }
+
+    public void activerLampe(int indiceLampe){
+        this.lampes.get(indiceLampe).allumer();
+    }
+
+    public void desactiverLampe(int indiceLampe){
+        this.lampes.get(indiceLampe).eteindre();
+    }
+
+    public void activertout (){
+        for (int i=0; i<lampes.size(); i++){
+            this.lampes.get(i).allumer();
+        }
+    }
+
+    public String toString (){
+        String aff = "Etat des lampes : \n";
+        for (int i=0; i<lampes.size(); i++){
+            aff += this.lampes.get(i).toString();
+        }
+        return aff;
+    }
+}
