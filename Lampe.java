5c718675f5ca363f32746e419519195673e9a8aa
Guiffault Elian <guiffaul2u@etu.univ-lorraine.fr>
2022-04-06 11:10:33 +0200

    Classe Lampe

--- Lampe.java
+++ Lampe.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Lampe {
+public class Lampe {
+    private String nom;
+    private boolean allume;
+
+    public Lampe (String n){
+        this.nom = n;
+        this.allume = false;
+    }
+
+    public void allumer(){
+        this.allume = true;
+    }
+
+    public void eteindre(){
+        this.allume = false;
+    }
 }
